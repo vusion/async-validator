6684f41c6e7e56ce02f153be2295698c03b34a85
haoxutong <1255965516@qq.com>
2017-12-26 22:43:59 +0800

    :sparkles: Add webpack build

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require('path');  //加载nodejs的路径处理模块
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),        //__dirname是一个nodejs变量，表示当前js文件所在的目录
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,    //配置文件目录下的es6文件夹作为js源代码文件夹
+                loader: 'babel-loader' ,
+            }
+        ]
+    }
+};
